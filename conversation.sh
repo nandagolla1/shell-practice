6eccfe5fa06dde865624d82d508bbdc1bb95e340
nanda <nandagolla074@gmail.com>
2025-05-18 16:55:10 +0530

    conversation between musk and trump

---
+++ conversation.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+echo "trump:: hey musk, how are you"
+echo "musk:: I'm fine trmp, how are you doing?"
+echo "trump:: I'm fine too. what's going on?"
+echo "musk:: nothing, i'm working on a mission, which makes us travel to mars."
+echo "trump:: it's excites me more. i'm astrophysics enthusiastic."
+ 
