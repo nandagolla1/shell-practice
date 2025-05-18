302281fd1d593896a6cdd7bc4fb841f4efda4fa5
nanda <nandagolla074@gmail.com>
2025-05-18 17:07:36 +0530

    variables shell script

---
+++ variables.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+person1=trump
+person2=musk
+
+echo $person1:: hey=$person2, how are you"
+echo =$person2:: I'm fine trmp, how are you doing?"
+echo $person1:: I'm fine too. what's going on?"
+echo =$person2:: nothing, i'm working on a mission, which makes us travel to mars."
+echo $person1:: it's excites me more. i'm astrophysics enthusiastic."
+ 
