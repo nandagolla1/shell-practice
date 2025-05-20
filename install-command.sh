299428e3c3f1f755ecd15814e11969c6f6d35044
nanda <nandagolla074@gmail.com>
2025-05-20 23:24:58 +0530

    data types

--- install-command.sh
+++ install-command.sh
@@ -2,33 +2,38 @@
 
 USERID=$(id -u)
 PACKAGES=("$@")
+RED="\e[31m"
+GREEN="\e[32m"
+YELLOW="\e[33m"
+BLUE="\e[34m"
+RESET="\e[0m"
 
 if [ $USERID -ne 0 ]
 then
-    echo "Error: please use super user previlages to run the script"
+    echo -e "${RED}Error: please use super user previlages to run the script ${RESET}"
     exit 1
 else
-    echo "your are run the script with root user."
+    echo -e "${GREEN}your are run the script with root user.${RESET}"
 fi
 
 PACKAGE_INSTALLER(){
     dnf list installed $1
     if [ $? -ne 0 ]
     then
-        echo "$1 is not installed, going to install it..."
+        echo -e "$1 is not installed, going to install it...${GREEN}Installing...${RESET}"
 
 
         dnf install $1 -y
         if [ $? -ne 0 ]
         then
-            echo "$1 not installed..."
+            echo -e "${RED}$1 not installed...${RESET}"
             exit 1
         else
-            echo "$1 is installed...."
+            echo -e "$1 is ${GREEN}installed....${RESET}"
         fi
 
     else
-        echo "$1 already installed...."
+        echo -e "$1 already ${GREEN}installed....${RESET}"
     fi
 }
 
