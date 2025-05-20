b6049417d12d686bc2fd712f444e68f823add497
nanda <nandagolla074@gmail.com>
2025-05-20 16:49:13 +0530

    data types

---
+++ pass-arguments-variables.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+person1=$1
+person2=$2
+
+echo "$person1:: hey $person2, how are you"
+echo "$person2:: I'm fine trmp, how are you doing?"
+echo "$person1:: I'm fine too. what's going on?"
+echo "$person2:: nothing, i'm working on a mission, which makes us travel to mars."
+echo "$person1:: it's excites me more. i'm astrophysics enthusiastic."
+ 
