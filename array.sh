b6ca03510925c70a574e70edd5a30c946bdf7dce
nanda <nandagolla074@gmail.com>
2025-05-20 16:26:35 +0530

    data types

--- array.sh
+++ array.sh
@@ -1,5 +1,11 @@
 #!/bin/bash
 
-MOVIES=("temper", "jalsa", "billa", "super")
+MOVIES=("temper" "jalsa" "billa" "super")
 
 echo "first movie : ${MOVIES[0]}"
+
+echo "second movie : ${MOVIES[1]"}
+
+echo "All movie : ${MOVIES[@]}"
+
+echo "tenth movie : ${MOVIES[10]}"
