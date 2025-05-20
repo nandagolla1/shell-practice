a4663ab71adcb3cc282786935c0c5ca71e214962
nanda <nandagolla074@gmail.com>
2025-05-20 16:08:39 +0530

    data types

---
+++ data-types.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+NUMBER1=100
+NUMBER2=200
+
+TIMESTAMP=$(date)
+echo "script executed at : $TIMESTAMP"
+
+sum=$($NUMBER1+$NUMBER2)
+
+echo "the sum of $NUMBER1 and $NUMBER2 is : $sum"
