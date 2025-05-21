860393975661f75e41ab5f07d70f2d5e0d1131fc
nanda <nandagolla074@gmail.com>
2025-05-21 16:55:38 +0530

    data types

--- install-command.sh
+++ install-command.sh
@@ -19,30 +19,30 @@ mkdir -p $LOGS_FOLDER
 
 if [ $USERID -ne 0 ]
 then
-    echo -e "${RED}Error: please use super user previlages to run the script ${RESET}" &>> $LOG_FILE
+    echo -e "${RED}Error: please use super user previlages to run the script ${RESET}" | tee -a $LOG_FILE
     exit 1
 else
-    echo -e "${GREEN}your are run the script with root user.${RESET}" &>> $LOG_FILE
+    echo -e "${GREEN}your are run the script with root user.${RESET}" | tee -a $LOG_FILE
 fi
 
 PACKAGE_INSTALLER(){
-    dnf list installed $1 &>> $LOG_FILE
+    dnf list installed $1 | tee -a $LOG_FILE
     if [ $? -ne 0 ]
     then
-        echo -e "$1 is not installed, going to install it...${GREEN}Installing...${RESET}" &>> $LOG_FILE
+        echo -e "$1 is not installed, going to install it...${GREEN}Installing...${RESET}" | tee -a $LOG_FILE
 
 
-        dnf install $1 -y &>> $LOG_FILE
+        dnf install $1 -y | tee -a $LOG_FILE
         if [ $? -ne 0 ]
         then
-            echo -e "${RED}$1 not installed...${RESET}" &>> $LOG_FILE
+            echo -e "${RED}$1 not installed...${RESET}" | tee -a $LOG_FILE
             exit 1
         else
-            echo -e "$1 is ${GREEN}installed....${RESET}" &>> $LOG_FILE
+            echo -e "$1 is ${GREEN}installed....${RESET}" | tee -a $LOG_FILE
         fi
 
     else
-        echo -e "$1 already ${GREEN}installed....${RESET}" &>> $LOG_FILE
+        echo -e "$1 already ${GREEN}installed....${RESET}" | tee -a $LOG_FILE
     fi
 }
 
